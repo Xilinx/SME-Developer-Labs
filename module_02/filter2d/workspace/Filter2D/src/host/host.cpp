9509f741830920cd88ee3459598b3a2f3ee85d2e
ThomasXilinx <tbollaer@xilinx.com>
2018-04-26 17:56:14 -0700

    Update host.cpp

--- module_02/filter2d/workspace/Filter2D/src/host/host.cpp
+++ module_02/filter2d/workspace/Filter2D/src/host/host.cpp
@@ -352,7 +352,7 @@ int main(int argc, char** argv)
 		          << (double) numRuns*3*nbytes / cpu_duration.count() / (1024.0*1024.0)
 		          << " MB/s" << std::endl;
 
-		std::cout << "FPGA Speedup:    " << cpu_duration.count() / fpga_duration.count() << "x" << std::endl;
+		std::cout << "FPGA Speedup:    " << cpu_duration.count() / fpga_duration.count() << " x" << std::endl;
 	}
 
 	// Release allocated memory
